13b63f0e0a96afdf7f899173a3a54ce75864be47
Алена Маслова <alena09062004@gmail.com>
2024-10-12 02:06:19 +0400

    start 2 lab. ready 3 task

---
+++ second_lab/lab2-3.asm
@@ -0,0 +1,27 @@
+;2^ctg(x)=a
+;x = arctan(1/log2(a))
+;полиз  x = 1 a log2  / arctan
+
+
+section .rodata
+a: dd 8.0
+
+
+section .bss
+x: resd 1
+
+
+section .text
+global main
+main:
+    fld1
+    fld1
+    fld1
+    fld dword[a]
+    fyl2x
+    fdiv
+    fpatan
+    fstp dword[x]
+    
+    xor rax, rax
+    ret
